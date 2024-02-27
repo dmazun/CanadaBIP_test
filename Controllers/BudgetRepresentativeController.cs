a4b627f60d337967a7451f0028ce82b72704df94
dmazun <dmytro.mazun@l-o-g-i-c.com>
2024-02-27 22:57:20 +0200

    save budget rep with null value

--- Controllers/BudgetRepresentativeController.cs
+++ Controllers/BudgetRepresentativeController.cs
@@ -205,18 +205,52 @@ namespace CanadaBIP_test.Controllers
             cmd.Parameters.Add(new SqlParameter("@Event_Name", SqlDbType.NVarChar) { Value = model.Event_Name });
             cmd.Parameters.Add(new SqlParameter("@Product", SqlDbType.NVarChar) { Value = model.Product });
             cmd.Parameters.Add(new SqlParameter("@Initiative_ID", SqlDbType.Int) { Value = model.Initiative_ID });
-            cmd.Parameters.Add(new SqlParameter("@Note", SqlDbType.NVarChar) { Value = model.Note });
-            cmd.Parameters.Add(new SqlParameter("@Type", SqlDbType.NVarChar) { Value = model.Type });
-            cmd.Parameters.Add(new SqlParameter("@Event_Type", SqlDbType.NVarChar) { Value = model.Event_Type });
-            cmd.Parameters.Add(new SqlParameter("@Attendance", SqlDbType.NVarChar) { Value = model.Attendance });
-            cmd.Parameters.Add(new SqlParameter("@Shared_Individual", SqlDbType.NVarChar) { Value = model.Shared_Individual });
             cmd.Parameters.Add(new SqlParameter("@Amount_Allocated", SqlDbType.Decimal) { Value = model.Amount_Allocated });
-            cmd.Parameters.Add(new SqlParameter("@Customer_ID", SqlDbType.NVarChar) { Value = model.Customer_ID });
-            cmd.Parameters.Add(new SqlParameter("@Customer_Count", SqlDbType.Int) { Value = model.Customer_Count });
-            cmd.Parameters.Add(new SqlParameter("@Customer_Type", SqlDbType.NVarChar) { Value = model.Customer_Type });
-            cmd.Parameters.Add(new SqlParameter("@FCPA_Veeva_ID", SqlDbType.NVarChar) { Value = model.FCPA_Veeva_ID });
-            cmd.Parameters.Add(new SqlParameter("@Account_ID", SqlDbType.NVarChar) { Value = model.Account_ID });
-            cmd.Parameters.Add(new SqlParameter("@Tier", SqlDbType.Int) { Value = model.Tier });
+
+            cmd.Parameters.Add(new SqlParameter("@Note", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Note) ? (object)DBNull.Value : model.Note
+            });
+            cmd.Parameters.Add(new SqlParameter("@Type", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Type) ? (object)DBNull.Value : model.Type
+            });
+            cmd.Parameters.Add(new SqlParameter("@Event_Type", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Event_Type) ? (object)DBNull.Value : model.Event_Type
+            });
+            cmd.Parameters.Add(new SqlParameter("@Attendance", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Attendance) ? (object)DBNull.Value : model.Attendance
+            });
+            cmd.Parameters.Add(new SqlParameter("@Shared_Individual", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Shared_Individual) ? (object)DBNull.Value : model.Shared_Individual
+            });
+            cmd.Parameters.Add(new SqlParameter("@Customer_ID", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Customer_ID) ? (object)DBNull.Value : model.Customer_ID
+            });
+            cmd.Parameters.Add(new SqlParameter("@Customer_Count", SqlDbType.Int)
+            {
+                Value = model.Customer_Count.HasValue ? (object)DBNull.Value : model.Customer_Count
+            });
+            cmd.Parameters.Add(new SqlParameter("@Customer_Type", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Customer_Type) ? (object)DBNull.Value : model.Customer_Type
+            });
+            cmd.Parameters.Add(new SqlParameter("@FCPA_Veeva_ID", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.FCPA_Veeva_ID) ? (object)DBNull.Value : model.FCPA_Veeva_ID
+            });
+            cmd.Parameters.Add(new SqlParameter("@Account_ID", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Account_ID) ? (object)DBNull.Value : model.Account_ID
+            });
+            cmd.Parameters.Add(new SqlParameter("@Tier", SqlDbType.Int)
+            {
+                Value = model.Tier.HasValue ? (object)DBNull.Value : model.Tier
+            });
 
             SqlParameter outputParameter = new SqlParameter
             {
@@ -246,18 +280,52 @@ namespace CanadaBIP_test.Controllers
             cmd.Parameters.Add(new SqlParameter("@Event_Name", SqlDbType.NVarChar) { Value = model.Event_Name });
             cmd.Parameters.Add(new SqlParameter("@Product", SqlDbType.NVarChar) { Value = model.Product });
             cmd.Parameters.Add(new SqlParameter("@Initiative_ID", SqlDbType.Int) { Value = model.Initiative_ID });
-            cmd.Parameters.Add(new SqlParameter("@Note", SqlDbType.NVarChar) { Value = model.Note });
-            cmd.Parameters.Add(new SqlParameter("@Type", SqlDbType.NVarChar) { Value = model.Type });
-            cmd.Parameters.Add(new SqlParameter("@Event_Type", SqlDbType.NVarChar) { Value = model.Event_Type });
-            cmd.Parameters.Add(new SqlParameter("@Attendance", SqlDbType.NVarChar) { Value = model.Attendance });
-            cmd.Parameters.Add(new SqlParameter("@Shared_Individual", SqlDbType.NVarChar) { Value = model.Shared_Individual });
             cmd.Parameters.Add(new SqlParameter("@Amount_Allocated", SqlDbType.Decimal) { Value = model.Amount_Allocated });
-            cmd.Parameters.Add(new SqlParameter("@Customer_ID", SqlDbType.NVarChar) { Value = model.Customer_ID });
-            cmd.Parameters.Add(new SqlParameter("@Customer_Count", SqlDbType.Int) { Value = model.Customer_Count });
-            cmd.Parameters.Add(new SqlParameter("@Customer_Type", SqlDbType.NVarChar) { Value = model.Customer_Type });
-            cmd.Parameters.Add(new SqlParameter("@FCPA_Veeva_ID", SqlDbType.NVarChar) { Value = model.FCPA_Veeva_ID });
-            cmd.Parameters.Add(new SqlParameter("@Account_ID", SqlDbType.NVarChar) { Value = model.Account_ID });
-            cmd.Parameters.Add(new SqlParameter("@Tier", SqlDbType.Int) { Value = model.Tier });
+
+            cmd.Parameters.Add(new SqlParameter("@Note", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Note) ? (object)DBNull.Value : model.Note
+            });
+            cmd.Parameters.Add(new SqlParameter("@Type", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Type) ? (object)DBNull.Value : model.Type
+            });
+            cmd.Parameters.Add(new SqlParameter("@Event_Type", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Event_Type) ? (object)DBNull.Value : model.Event_Type
+            });
+            cmd.Parameters.Add(new SqlParameter("@Attendance", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Attendance) ? (object)DBNull.Value : model.Attendance
+            });
+            cmd.Parameters.Add(new SqlParameter("@Shared_Individual", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Shared_Individual) ? (object)DBNull.Value : model.Shared_Individual
+            });
+            cmd.Parameters.Add(new SqlParameter("@Customer_ID", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Customer_ID) ? (object)DBNull.Value : model.Customer_ID
+            });
+            cmd.Parameters.Add(new SqlParameter("@Customer_Count", SqlDbType.Int)
+            {
+                Value = model.Customer_Count == null ? (object)DBNull.Value : model.Customer_Count
+            });
+            cmd.Parameters.Add(new SqlParameter("@Customer_Type", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Customer_Type) ? (object)DBNull.Value : model.Customer_Type
+            });
+            cmd.Parameters.Add(new SqlParameter("@FCPA_Veeva_ID", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.FCPA_Veeva_ID) ? (object)DBNull.Value : model.FCPA_Veeva_ID
+            });
+            cmd.Parameters.Add(new SqlParameter("@Account_ID", SqlDbType.NVarChar)
+            {
+                Value = string.IsNullOrEmpty(model.Account_ID) ? (object)DBNull.Value : model.Account_ID
+            });
+            cmd.Parameters.Add(new SqlParameter("@Tier", SqlDbType.Int)
+            {
+                Value = model.Tier == null ? (object)DBNull.Value : model.Tier
+            });
 
             SqlParameter outputParameter = new SqlParameter
             {
